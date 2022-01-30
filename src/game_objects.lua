83f7b7a44abd1fafdd357809ec3b0638b73f0f97
badjem79 <badjem79@gmail.com>
2022-01-30 08:04:27 +0100

    first commit whit healing hearts

---
+++ src/game_objects.lua
@@ -0,0 +1,51 @@
+--[[
+    GD50
+    Legend of Zelda
+
+    Author: Colton Ogden
+    cogden@cs50.harvard.edu
+]]
+
+GAME_OBJECT_DEFS = {
+    ['switch'] = {
+        type = 'switch',
+        texture = 'switches',
+        frame = 2,
+        width = 16,
+        height = 16,
+        solid = false,
+        defaultState = 'unpressed',
+        states = {
+            ['unpressed'] = {
+                frame = 2
+            },
+            ['pressed'] = {
+                frame = 1
+            }
+        }
+    },
+    ['heart'] = {
+        type = 'heart',
+        texture = 'hearts',
+        frame = 2,
+        width = 16,
+        height = 16,
+        solid = false,
+        consumable = true,
+        defaultState = 'full',
+        states = {
+            ['star'] = {
+                frame = 6
+            },
+            ['full'] = {
+                frame = 5
+            },
+            ['half'] = {
+                frame = 3
+            }
+        }
+    },
+    ['pot'] = {
+        -- TODO
+    }
+}
